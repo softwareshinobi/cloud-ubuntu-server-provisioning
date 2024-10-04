e83fa1fe082bf1cfd38f41f5bacef73035f7b65e
Software Shinobi <the.software.shinobi@gmail.com>
2024-10-03 21:39:35 -0500

    automated terminal push

--- provision.bash
+++ provision.bash
@@ -41,13 +41,42 @@ apt-get install -y net-tools
 apt-get install -y vim
 
 ##
+##
 
-apt-get install -y docker-compose
+#set -e;
+
+#set -x;
+
+## updating apt
+
+sudo apt-get update;
 
 ##
 
-echo
-echo "## "
-echo "## routine / provision-ubuntu-basic / completed"
-echo "## "
-echo
+sudo apt -y install ca-certificates curl;
+
+### APT package dependency setup
+
+sudo install -m 0755 -d /etc/apt/keyrings
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+## docker engine install
+
+sudo apt update
+
+sudo apt purge -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose docker docker.io
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
+
+echo "finished provision..."
+
